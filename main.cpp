60fc37fc94cbf113193e893fe8896469be1bb0b0
Max <max.te.lintelo@gmail.com>
2021-09-15 17:03:07 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtCharts/QChartView>
+#include <QtCharts/QBarSeries>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QLegend>
+#include <QtCharts/QBarCategoryAxis>
+#include <QtCharts/QHorizontalBarSeries>
+#include <QtCharts/QLineSeries>
+#include <QtCharts/QCategoryAxis>
+
+QT_CHARTS_USE_NAMESPACE
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QBarSet *set0 = new QBarSet("Temperature");
+    QBarSet *set1 = new QBarSet("Humidity");
+    QBarSet *set2 = new QBarSet("Pressure");
+
+    *set0 << 283 << 341 << 313 << 338 << 346 << 335;
+    *set1 << 250 << 315 << 282 << 307 << 303 << 330;
+    *set2 << 294 << 246 << 257 << 319 << 300 << 325;
+
+    QBarSeries *series = new QBarSeries();
+
+    series->append(set0);
+    series->append(set1);
+    series->append(set2);
+
+    QChart *chart = new QChart();
+    chart->addSeries(series);
+    chart->setTitle("Batting Avg by Year");
+    chart->setAnimationOptions(QChart::AllAnimations);
+
+    QStringList categories;
+    categories << "2013" << "2014" << "2015" << "2016" << "2017" << "2018";
+
+    QBarCategoryAxis *axis = new QBarCategoryAxis();
+    axis->append(categories);
+    chart->createDefaultAxes();
+    chart->setAxisX(axis, series);
+    chart->legend()->setVisible(true);
+    chart->legend()->setAlignment(Qt::AlignBottom);
+
+    QChartView *chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+
+    QPalette pal = qApp->palette();
+    pal.setColor(QPalette::Window, QRgb(0xffffff));
+    pal.setColor(QPalette::WindowText, QRgb(0x404044));
+    qApp->setPalette(pal);
+
+    QMainWindow window;
+
+    window.setCentralWidget(chartView);
+    window.resize(420, 300);
+    window.show();
+
+    return a.exec();
+}
